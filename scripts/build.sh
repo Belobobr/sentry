08c529b4357eaf25e41e6545711d89a6b5a10691
Mikhail Sednev <sednevmixail@gmail.com>
2018-10-09 14:26:21 +0300

    temp

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,6 @@
 
 if [[ -z $(git status -s) ]]
 then
-    cross-env NODE_ENV=production
-    cross-env RELEASE_VERSION=\"$(git rev-parse HEAD)\"
     webpack --config webpack.production.js
     npm run sentry_update_release
 else
